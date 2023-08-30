db4a2c1dc0b456572a80cf14bf53a251f7b87e7c
Athira K <athira2864@gmail.com>
2023-08-30 13:04:20 +0530

    first commit

---
+++ demo/app/View/Components/form.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Form</h1>
+    <form action="">
+        <label for ="name">Full Name</label>
+        <input type="text" id="name" name="name"> 
+        <label for ="age">Age</label>
+        <input type="text" id="age" name="age"> 
+        <button type="button" >Submit</button>
+</body>
+</html>
